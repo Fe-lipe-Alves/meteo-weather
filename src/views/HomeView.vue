1c9092f75217bed1983037488d70e9de2986f403
Felipe Alves <felipe.aalves@connectcom.com.br>
2023-05-06 12:34:27 -0300

    Criando arquivo de componente da tela principal

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <HeaderPage/>
+    <main>
+        <div class="w-full">
+            <div class="w-6/12 mx-auto flex">
+                <div>
+                    <img src="https://files.readme.io/c3d2596-weather_icon_small_ic_mostly_clear5x.png" alt="">
+                </div>
+            </div>
+            
+        </div>
+    </main>
+</template>
+
+<script lang="ts" setup>
+import HeaderPage from "@/components/HeaderPage.vue";
+</script>
+
+<style scoped>
+
+</style>
