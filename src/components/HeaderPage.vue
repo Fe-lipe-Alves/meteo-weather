6a28cdc4b1d6cd7fc1e6adae65e3044a87eca5f6
Felipe Alves <felipe.aalves@connectcom.com.br>
2023-05-06 12:33:47 -0300

    Criando componente cabeçalho da página

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="w-10/12 mx-auto flex justify-between items-center py-8">
+    <div>
+        <h1>MeteoWeather</h1>
+    </div>
+
+    <div>
+        <InputSearch/>
+    </div>
+
+    <div>
+        <span>06/05/2023</span>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import InputSearch from "@/components/InputSearch.vue";
+</script>
+
+<style scoped>
+
+</style>
