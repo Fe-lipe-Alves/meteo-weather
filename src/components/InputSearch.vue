48a314ff5fa0c5bb50ba9f0c4d8057e4f9827b0b
Felipe Alves <felipealves1550@gmail.com>
2023-05-11 18:52:34 -0300

    Criando compontentes de pesquisa e cabeçalho

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -7,13 +7,14 @@
             type="text"
             value="Presidente Prudente"
             autocomplete="off"
-            class="w-full rounded-[1.4rem] shadow px-8 py-2 text-center text-lg focus:outline-none"
+            class="w-full rounded-[1.4rem] shadow px-8 py-2 text-center text-lg focus:outline-none bg-cultured/70"
+            :class="{'bg-white': isFocus}"
             @focusin="setFocus(true)"
             @focusout="setFocus(false)"
         >
 
         <div
-            class="w-full mt-2 list bg-inherit absolute rounded-b-[1.4rem] overflow-hidden"
+            class="w-full mt-2 list bg-inherit absolute rounded-b-[1.4rem] overflow-hidden z-10"
             v-show="isFocus"
         >
             <ul>
@@ -41,7 +42,7 @@ const cities = [
 ]
 
 const search = ref('')
-const isFocus = ref(true)
+const isFocus = ref(false)
 
 function setFocus(value) {
     isFocus.value = value
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,31 @@
     <HeaderPage/>
     <main>
         <div class="w-full">
-            <div class="w-6/12 mx-auto flex">
-                <div>
-                    <img src="https://files.readme.io/c3d2596-weather_icon_small_ic_mostly_clear5x.png" alt="">
+            <div class="w-full mx-auto flex flex-col item-center gap-4 py-4">
+                <div class="flex justify-center gap-16">
+                    <div class="flex justify-center items-center">
+                        <img src="./../assets/tomorrow-icons/v2/large/png/10001_clear_large.png" alt="">
+                    </div>
+
+                    <div>
+                        <div class="h-fit flex justify-center items-center px-8 gap-4 py-2 bg-zinc-50 rounded-full backdrop-blur-sm ">
+                            <span class="text-4xl">32°</span>
+                            <span class="border-l w-[1px] h-4 border-black-olive"></span>
+                            <span>Chuva leve</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w-full flex justify-center">
+                    <div class="bg-cultured mt-4 flex gap-8 text-center font-medium text-black-olive text-sm px-8 py-1 rounded-md">
+                        <span>Sensação 30°</span>
+                        <span>Vento 5 km/h</span>
+                        <span>Umidade 60%</span>
+                        <span>Chance de chuva 5%</span>
+                    </div>
                 </div>
             </div>
+
             
         </div>
     </main>
