9e4d88ec15e368d6f102b614989638a34d3ed6d3
Felipe Alves <felipealves1550@gmail.com>
2023-06-04 19:35:02 -0300

    Fixando tamanho da imagem

--- src/components/hourly/HourlyHour.vue
+++ src/components/hourly/HourlyHour.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div>
-    <img src="@/assets/tomorrow-icons/v2/small/png/10000_clear_small.png" alt="Chuva leve">
+    <img src="@/assets/tomorrow-icons/v2/small/png/10000_clear_small.png" alt="Chuva leve" width="24" height="24">
     <span>24°</span>
     <small>12:00</small>
   </div>
