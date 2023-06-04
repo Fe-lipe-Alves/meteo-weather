e8ada33f33e2a6ff794bd11d00a1650708997348
Felipe Alves <felipealves1550@gmail.com>
2023-06-04 19:45:22 -0300

    Adicionando rodapé

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,8 @@
     <HourlyBox/>
     <WeekBox/>
   </main>
+
+  <FooterPage />
 </template>
 
 <script lang="ts" setup>
@@ -13,6 +15,7 @@ import HeaderPage from "@/components/HeaderPage.vue";
 import HourlyBox from "@/components/hourly/HourlyBox.vue";
 import CurrentBox from "@/components/current/CurrentBox.vue";
 import WeekBox from "@/components/week/WeekBox.vue";
+import FooterPage from "@/components/FooterPage.vue";
 </script>
 
 <style scoped>
