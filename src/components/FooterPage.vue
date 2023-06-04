064e05499026cf6fdf927fd3fd510ff7b83c296b
Felipe Alves <felipealves1550@gmail.com>
2023-06-04 19:45:10 -0300

    Criando rodapé da página

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="w-8/12 mx-auto flex grid-cols-3 justify-between items-center text-xs pb-1 text-cultured">
+    <p>Fonte meteorológica <a href="https://www.tomorrow.io">Tomorrow.io</a></p>
+    <p>Desenvolvido por <a href="https://github.com/Fe-lipe-Alves">Felipe Alves</a></p>
+    <p>Imagem <a href="">Gregore Taylor</a> por <a href="">Unsplash</a></p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
