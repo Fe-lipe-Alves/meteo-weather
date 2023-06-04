fdf01204c0f72e5cd1c14b7f25682221dc3e88b1
Felipe Alves <felipealves1550@gmail.com>
2023-06-04 19:10:26 -0300

    Extraindo componentes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,16 @@
 <template>
   <HeaderPage/>
+
   <main>
-    <div class="w-full">
-      <div class="w-full mx-auto flex flex-col items-center gap-4">
-        <CurrentIcon code="40010_rain_large" description="Ícone de chuva leve" />
-
-        <CurrentWeather description="Chuva leve" degrees="32" />
-
-        <CurrentDetailsList />
-        </div>
-    </div>
+    <CurrentBox/>
+    <HourlyBox/>
   </main>
 </template>
 
 <script lang="ts" setup>
 import HeaderPage from "@/components/HeaderPage.vue";
-import CurrentIcon from "@/components/current/CurrentIcon.vue";
-import CurrentWeather from "@/components/current/CurrentWeather.vue";
-import CurrentDetailsList from "@/components/current/CurrentDetailsList.vue";
+import HourlyBox from "@/components/hourly/HourlyBox.vue";
+import CurrentBox from "@/components/current/CurrentBox.vue";
 </script>
 
 <style scoped>
