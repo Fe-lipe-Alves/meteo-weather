cbfd7a10e8e87b78edc865053c17eab7a07be7c9
Felipe Alves <felipealves1550@gmail.com>
2023-06-04 19:09:44 -0300

    Criando componentes de hora a hora

---
+++ src/components/current/CurrentBox.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CurrentWeather from "@/components/current/CurrentWeather.vue";
+import CurrentDetailsList from "@/components/current/CurrentDetailsList.vue";
+import CurrentIcon from "@/components/current/CurrentIcon.vue";
+</script>
+
+<template>
+  <section class="w-full">
+    <div class="w-full mx-auto flex flex-col items-center gap-4">
+      <CurrentIcon code="40010_rain_large" description="Ícone de chuva leve"/>
+
+      <CurrentWeather description="Chuva leve" degrees="32"/>
+
+      <CurrentDetailsList/>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/hourly/HourlyBox.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import HourlyGroupDay from "@/components/hourly/HourlyGroupDay.vue";
+</script>
+
+<template>
+  <section class="w-full">
+    <div class="w-10/12 lg:w-8/12 bg-cultured mx-auto flex flex-col my-8 py-2 px-4 rounded-md">
+      <HourlyGroupDay />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
