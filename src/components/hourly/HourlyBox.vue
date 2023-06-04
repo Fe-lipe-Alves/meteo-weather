a5e0fcf34469876e3ec5e99652a2053c0803da3d
Felipe Alves <felipealves1550@gmail.com>
2023-06-04 19:34:34 -0300

    Removendo margens

--- src/components/hourly/HourlyBox.vue
+++ src/components/hourly/HourlyBox.vue
@@ -5,7 +5,7 @@ import HourlyGroupDay from "@/components/hourly/HourlyGroupDay.vue";
 
 <template>
   <section class="w-full">
-    <div class="w-10/12 lg:w-8/12 bg-cultured mx-auto flex flex-col my-8 py-2 px-4 rounded-md">
+    <div class="w-10/12 lg:w-8/12 bg-cultured mx-auto flex flex-col py-2 px-4 rounded-md">
       <HourlyGroupDay />
     </div>
   </section>
